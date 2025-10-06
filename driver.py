776df5375b49817515986dcaf5fc9102acbfa356
Vy D Nguyen <vynguyen@Vys-MacBook-Pro.local>
2025-10-05 23:54:28 -0400

    Create a driver model class

---
+++ driver.py
@@ -0,0 +1,38 @@
+from enums import AvailabilityStatus, RideStatus
+from user import User
+
+class Driver(User):
+    def __init__(self, name, email, phone, user_id, current_location, driver_id, availability_status=AvailabilityStatus.OFFLINE):
+        super().__init__(name, email, phone, user_id, current_location)
+        self.driver_id = driver_id
+        self.availability_status = availability_status
+        self.current_ride_status = None  # Track the current ride request
+
+    def set_availability(self, status):
+        if isinstance(status, AvailabilityStatus):
+            self.availability_status = status
+    
+    def receive_ride_request(self, ride_status=RideStatus.REQUESTED):
+        self.current_ride_status = ride_status
+
+    def accept_ride(self):
+        self.current_ride_status = RideStatus.ACCEPTED
+        print("Ride accepted.")
+    
+    def cancel_ride(self):
+        self.current_ride_status = RideStatus.CANCELLED
+        print("Ride cancelled.")
+
+    def start_ride(self):
+        self.current_ride_status = RideStatus.STARTED
+    
+    def navigate_to_location(self,location):
+        print(f"Navigating to {location}.")
+    
+    def confirm_pickup(self):
+        self.current_ride_status = RideStatus.PICK_UP
+        print("Pickup confirmed.")
+    
+    def complete_trip(self):
+        self.current_ride_status = RideStatus.COMPLETED
+        print("Trip completed.")
