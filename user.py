ba0d7a2bd7c0f53dcd5d6b1630081ba546d3909a
Vy D Nguyen <vynguyen@Vys-MacBook-Pro.local>
2025-10-08 19:58:00 -0400

    delete contructor in abstract method and make update location abstract

--- user.py
+++ user.py
@@ -2,9 +2,12 @@ from abc import ABC, abstractmethod
 
 class User(ABC):
     @abstractmethod
-    def __init__(self, name, email, phone, user_id):
+    def get_info(self, name, email, phone, user_id):
         self.name = name
-        self.email = email
+        self.email = email 
         self.phone = phone
         self.user_id = user_id
+    
+    def update_location(self, new_location):
+        pass
     
