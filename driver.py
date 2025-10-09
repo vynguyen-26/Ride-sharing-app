a12b6bdb85cea740c90138d3bb68de214872afdf
Vy D Nguyen <vynguyen@Vys-MacBook-Pro.local>
2025-10-08 20:00:30 -0400

    Delete hard-code rider status and add override update location method

--- driver.py
+++ driver.py
@@ -3,12 +3,15 @@ from ride_status import RideStatus
 from user import User
 
 class Driver(User):
-    def __init__(self, name, email, phone, user_id, current_location, driver_id, driver_status=DriverStatus.OFFLINE):
-        super().__init__(name, email, phone, user_id)
+    def get_info(self, name, email, phone, user_id, driver_id, current_loation, driver_status):
+        super().get_info(name, email, phone, user_id)
         self.driver_id = driver_id
-        self.driver_status = driver_status
         self.current_location = current_location
+        self.driver_status = driver_status
         self.current_ride_status = None  # Track the current ride request
+
+    def update_location(self, new_location):
+        self.current_location = new_location
 
     def set_availability(self, status):
         if isinstance(status,DriverStatus):
